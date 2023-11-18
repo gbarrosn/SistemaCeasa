bc6a214e31b1e75b509d9ab9fcd90e1fbb44711b
gbarrosn <gbarros.n@gmail.com>
2023-11-18 18:50:47 -0300

    salvando

--- dist/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Interface"},{"l":"Login"}];updateSearchResults();
--- src/Login/Login.java
+++ src/Login/Login.java
@@ -15,8 +15,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Login {
     private String username;
